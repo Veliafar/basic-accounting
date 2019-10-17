9a2dcf08c64c4cb70e6722f57de56b680ee502aa
Veliafar <veliafar.seo@google.com>
2019-10-17 18:22:53 +0300

    login validation ready

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -14,21 +14,14 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
     this.form = new FormGroup({
-      'email': new FormControl(null, [
-        Validators.required,
-        Validators.email
-      ]),
-      'password': new FormControl(null, [
-        Validators.required,
-        Validators.minLength(6)
-      ])
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'password': new FormControl(null, [Validators.required,Validators.minLength(6)])
     })
+
   }
 
-  onSubmit() { 
-
+  onSubmit() {
     console.log(this.form);
-    
   }
 
 }
